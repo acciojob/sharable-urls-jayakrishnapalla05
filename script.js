e6ce3d377c5c3d79ce41325ca15e4ef31172d6e2
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-06-27 13:05:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // your code here
+function update(){
+	const heading=document.getElementById('url');
+	const name=document.getElementById("name").value;
+    const year=document.getElementById("year").value;
+	let url='';
+
+	if(name === '' && year === ''){
+		url="https://localhost:8080/";
+	}
+	else if(name! == '' && year === '')
+	{
+		url="https://localhost:8080/"+"?name="+name;
+	}
+	else if(name === '' && year!== ''){
+		url="https://localhost:8080/"+"?year="+year;
+	}
+	else if(name!=='' && year !== ''){
+		url="https://localhost:8080/"+"?name="+name+"&year="+year;
+	}
+	heading.textContent='url';
+}
