6cda7802e0b18230833d81c7aa2ca35260263316
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-06-27 13:12:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 // your code here
-function update(){
-	let heading=document.getElementById('url');
-	let name=document.getElementById("name").value;
-    let year=document.getElementById("year").value;
-	let url='';
+ document.getElementById("urlForm").addEventListener("submit", function(event) {
+      event.preventDefault();
 
-	if(name === '' && year === ''){
-		url="https://localhost:8080/";
-	}
-	else if(name! == '' && year === '')
-	{
-		url="https://localhost:8080/"+"?name="+name;
-	}
-	else if(name === '' && year!== ''){
-		url="https://localhost:8080/"+"?year="+year;
-	}
-	else if(name!=='' && year !== ''){
-		url="https://localhost:8080/"+"?name="+name+"&year="+year;
-	}
-	heading.textContent=url;
-}
+      // Get input values
+      var name = document.getElementById("name").value;
+      var year = document.getElementById("year").value;
+
+      // Generate query string
+      var queryString = "name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
+
+      // Update URL text
+      document.getElementById("url").textContent = "https://localhost:8080/?" + queryString;
+    });
